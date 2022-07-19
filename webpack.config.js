d3cfde8d6ba91c0e91e227e7b2e5dcf1224f06b3
unknown <abrahak81@gmail.com>
2022-07-19 23:02:17 +0300

    Add leaderboard to html

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,72 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  devServer: {
+
+    static: './dist',
+
+  },
   plugins: [
 
     new HtmlWebpackPlugin({
-
-      title: 'Output Management',
 
     }),
 
   ],
   output: {
-    filename: 'main.js',
+    filename: "[name].js",
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
   },
-  mode: 'development'
+  optimization: {
+
+    runtimeChunk: 'single',
+
+  },
+  mode: 'development',
+  module: {
+
+    rules: [
+
+      {
+
+        test: /\.css$/i,
+
+        use: ['style-loader', 'css-loader'],
+
+      },
+      {
+
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+        type: 'asset/resource',
+
+      },
+      {
+
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+
+        type: 'asset/resource',
+
+      },
+      {
+
+        test: /\.(csv|tsv)$/i,
+
+        use: ['csv-loader'],
+
+      },
+
+      {
+
+        test: /\.xml$/i,
+
+        use: ['xml-loader'],
+
+      },
+
+    ],
+
+  },
 };
