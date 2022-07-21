4695d07cb2c9c9924256f027c8e8c2c58d7acde9
unknown <abrahak81@gmail.com>
2022-07-21 16:51:07 +0300

    Add create game

--- src/modules/createGame.js
+++ src/modules/createGame.js
@@ -7,4 +7,18 @@ const getID = () => {
     if(window.localStorage.getItem('id')){
         string = window.localStorage.getItem('id').slice(14,35);
     }
-}
+    return string
+};
+
+const creatGame = async (data) => {
+    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: data,
+    });
+    response.json().then((object)=>{
+        setID(object);
+    });
+};
