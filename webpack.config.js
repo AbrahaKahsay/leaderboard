7698c74a6ff973266cf1f0844fd712d7f79be237
unknown <abrahak81@gmail.com>
2022-07-22 08:46:46 +0300

    Reinsatll webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,34 +3,22 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   devServer: {
 
     static: './dist',
 
   },
-  experiments: {
-    topLevelAwait: true,
-
-  },
-  plugins: [
-
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-
-  ],
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/',
   },
   optimization: {
 
     runtimeChunk: 'single',
 
   },
-  mode: 'development',
   module: {
 
     rules: [
@@ -43,36 +31,29 @@ module.exports = {
 
       },
       {
-
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-
         type: 'asset/resource',
-
       },
       {
-
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-
-        type: 'asset/resource',
-
-      },
-      {
-
         test: /\.(csv|tsv)$/i,
-
         use: ['csv-loader'],
-
       },
-
       {
-
         test: /\.xml$/i,
-
         use: ['xml-loader'],
-
       },
-
     ],
-
   },
+  experiments: {
+    topLevelAwait: true,
+  },
+
+  plugins: [
+
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+
+    }),
+
+  ],
 };
